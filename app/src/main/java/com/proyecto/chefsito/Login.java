5b5aa6a60ba35d039f6f1c8deb738c7656cfe3d7
Jaime Stiven Leon Tunjano <jaimeleonxd@gmail.com>
2019-10-05 19:09:59 -0500

    Pantalla Login
    Se creo la parte gráfica de la pantalla del login.

--- app/src/main/java/com/proyecto/chefsito/Login.java
+++ app/src/main/java/com/proyecto/chefsito/Login.java
@@ -3,12 +3,15 @@ package com.proyecto.chefsito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Login extends AppCompatActivity {
+    ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.image_logo);
+        logo.setImageResource(R.drawable.chefsito);
     }
 }
