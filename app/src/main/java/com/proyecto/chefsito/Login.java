97359913fbb6b7aab457569acf5c96f65de43c78
Jaime Stiven Leon Tunjano <jaimeleonxd@gmail.com>
2019-10-17 15:13:42 -0500

    Vista Cocina
    Creación vista receta, correcciones a la vista de inicio y vista de login.

--- app/src/main/java/com/proyecto/chefsito/Login.java
+++ app/src/main/java/com/proyecto/chefsito/Login.java
@@ -2,16 +2,28 @@ package com.proyecto.chefsito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
     ImageView logo;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         logo = findViewById(R.id.image_logo);
         logo.setImageResource(R.drawable.chefsito);
+        login = findViewById(R.id.boton_login);
+
     }
+    public void ingreso(View view){
+        Intent intent = new Intent(view.getContext(),Inicio.class);
+        startActivity(intent);
+    }
+
+
 }
